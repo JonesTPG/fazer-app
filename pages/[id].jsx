e12a37361d43dd6dce0bbf8d15ff8dd6185e3750
JonesTPG <joonas.ryynanen1@gmail.com>
2022-01-07 21:45:25 +0200

    epic changes.

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,27 +1,66 @@
-import { Stack } from "@mui/material";
+import { Stack, Fab, Typography } from "@mui/material";
+import ArrowCircleLeftTwoToneIcon from "@mui/icons-material/ArrowCircleLeftTwoTone";
+import ArrowCircleRightTwoToneIcon from "@mui/icons-material/ArrowCircleRightTwoTone";
+
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Home({ id }) {
+  const router = useRouter();
   return (
     <Stack
       sx={{
         justifyContent: "center",
         alignItems: "center",
-        boxShadow: "0 3px 10px rgb(255 255 255 / 0.2)",
-        p: 0.2,
       }}
-      data-testid="container"
+      direction="row"
+      spacing={4}
     >
-      <Image src={`/api/textTvImage/${id}`} alt="Text TV page" width={800} height={650} />
+      <Fab
+        color="secondary"
+        aria-label="add"
+        size="large"
+        onClick={() => router.push(`/${id - 1}`)}
+      >
+        <ArrowCircleLeftTwoToneIcon />
+      </Fab>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          p: 0.2,
+        }}
+        data-testid="container"
+      >
+        <Typography
+          sx={{
+            color: (theme) => theme.palette.common.white,
+            fontWeight: "600",
+          }}
+          variant="h2"
+        >
+          {id}
+        </Typography>
+        <Image src={`/api/textTvImage/${id}`} alt="Text TV page" width={800} height={650} />
+      </Stack>
+      <Fab
+        color="secondary"
+        aria-label="add"
+        size="large"
+        onClick={() => router.push(`/${Number(id) + 1}`)}
+      >
+        <ArrowCircleRightTwoToneIcon />
+      </Fab>
     </Stack>
   );
 }
+
 export async function getStaticProps({ params }) {
   return { props: { id: params.id }, revalidate: 300 };
 }
 
 export async function getStaticPaths() {
-  const paths = Array.from(Array(10).keys()).map((i) => ({ params: { id: `${i + 101}` } }));
+  const paths = Array.from(Array(10).keys()).map((i) => ({ params: { id: `${i + 100}` } }));
   return {
     paths,
     fallback: "blocking",
