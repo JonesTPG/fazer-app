947e80f7f2cc2a13165978eed8c38194bec28d02
Markus Röman <markus.roman@hotmail.fi>
2022-01-15 09:27:48 +0200

    feat(ui): input form styling

--- src/components/TextTvPageForm.jsx
+++ src/components/TextTvPageForm.jsx
@@ -1,14 +1,30 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import { useState } from "react";
+
 export default function TextTvPageForm() {
-  const handleSubmit = async (event) => {
+  const [values, setValues] = useState({ pageNumber: "", nickname: "", reason: "" });
+  const [open, setOpen] = useState(false);
+
+  const handleChange = (prop) => (event) => {
     event.preventDefault();
+    setValues((old) => ({ ...old, [prop]: event.target.value }));
+  };
 
-    const form = new FormData(event.target);
-    const formData = Object.fromEntries(form.entries());
-
-    console.log(formData);
+  const handleSubmit = async () => {
+    if (!pageNumber || !nickname) {
+      return null;
+    }
 
     const res = await fetch("/api/textTvPages", {
-      body: JSON.stringify(formData),
+      body: JSON.stringify(values),
       headers: {
         "Content-Type": "application/json",
       },
@@ -16,29 +32,72 @@ export default function TextTvPageForm() {
     });
 
     const result = await res.json();
-    console.log(result);
+    console.log("Saved: ", { result });
+    setOpen(false);
+    setValues({ pageNumber: "", nickname: "", reason: "" });
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="pageNumber" className="form-label">
-        PageNumber
-      </label>
-      <input name="pageNumber" type="text" className="form-control" />
-
-      <label htmlFor="header" className="form-label">
-        header
-      </label>
-      <input name="header" type="text" className="form-control" />
-
-      <label htmlFor="content" className="form-label">
-        content
-      </label>
-      <textarea name="content" type="text" className="form-control" />
-
-      <button className="btn btn-primary" type="submit">
-        Create TextTvPage
-      </button>
-    </form>
+    <>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Create a new text-tv page
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Create a new text-tv page</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2}>
+            <OutlinedInput
+              sx={{
+                borderRadius: 1,
+                color: "common.white",
+                bgcolor: "background.paper",
+              }}
+              placeholder="Page number*"
+              value={values.pageNumber}
+              onChange={handleChange("pageNumber")}
+              inputProps={{ sx: { p: 1.5 } }}
+            />
+            <OutlinedInput
+              sx={{
+                borderRadius: 1,
+                color: "common.white",
+                bgcolor: "background.paper",
+              }}
+              placeholder="Nickname*"
+              value={values.nickname}
+              onChange={handleChange("nickname")}
+              inputProps={{ sx: { p: 1.5 } }}
+            />
+            <OutlinedInput
+              sx={{
+                borderRadius: 1,
+                color: "common.white",
+                bgcolor: "background.paper",
+              }}
+              placeholder="Reason"
+              value={values.reason}
+              onChange={handleChange("reason")}
+              inputProps={{ sx: { p: 1.5 } }}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={() => handleSubmit()}>
+            Create
+          </Button>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
