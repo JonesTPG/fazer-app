f0aeecca644c0f0454a3834176574eabad068fc4
Markus Röman <markus.roman@hotmail.fi>
2022-01-15 22:57:41 +0200

    chore: packages and api fix

--- pages/api/textTvPages.ts
+++ pages/api/textTvPages.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { createFavoriteTextTvPage } from "../../src/lib/redis";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const id = await createFavoriteTextTvPage(JSON.parse(req.body));
+  const id = await createFavoriteTextTvPage(req.body);
   console.log(id);
   res.status(200).json({ id });
 };
